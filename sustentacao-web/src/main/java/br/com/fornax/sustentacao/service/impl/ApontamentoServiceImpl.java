8eadc2567f0ffe165b5fbf71f06cac19d0eca1d1
Lucas Henrique de Sousa <lucas.sousa@outlook.com>
2015-12-23 12:47:14 -0200

    Funcionalidade para contabilizar as horas disponiveis adicionada

--- sustentacao-web/src/main/java/br/com/fornax/sustentacao/service/impl/ApontamentoServiceImpl.java
+++ sustentacao-web/src/main/java/br/com/fornax/sustentacao/service/impl/ApontamentoServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.fornax.sustentacao.dao.ApontamentoDAO;
+import br.com.fornax.sustentacao.dao.TarefaDAO;
 import br.com.fornax.sustentacao.model.Apontamento;
+import br.com.fornax.sustentacao.model.Tarefa;
 import br.com.fornax.sustentacao.service.ApontamentoService;
 
 @Service
@@ -18,17 +20,25 @@ import br.com.fornax.sustentacao.service.ApontamentoService;
 public class ApontamentoServiceImpl implements ApontamentoService {
 
 	@Inject
-	private ApontamentoDAO dao;
+	private ApontamentoDAO apontamentoDAO;
+
+	@Inject
+	private TarefaDAO tarefaDAO;
 
 	@Override
 	public boolean cadastrarApontamento(Apontamento apontamento) {
-		List<Apontamento> lista = dao.listarApontamentoDoDia(apontamento.getDataApontamento(),
+		List<Apontamento> lista = apontamentoDAO.listarApontamentoDoDia(apontamento.getDataApontamento(),
 				apontamento.getHoraInicio(), apontamento.getHoraTermino());
 
 		if (lista.isEmpty()) {
-			apontamento.setDataCadastro(Calendar.getInstance());
-			dao.inserir(apontamento);
-			return true;
+			Tarefa tarefa = (Tarefa) tarefaDAO.buscarPorId(apontamento.getTarefa(), apontamento.getTarefa().getId());
+			int horasTrabalhadas = calcularHorasTrabalhadas(apontamento.getHoraTermino(), apontamento.getHoraInicio());
+			if (tarefa.getQtdHorasDisponiveis() - horasTrabalhadas >= 0) {
+				tarefa.setQtdHorasDisponiveis(tarefa.getQtdHorasDisponiveis() - horasTrabalhadas);
+				apontamento.setDataCadastro(Calendar.getInstance());
+				apontamentoDAO.inserir(apontamento);
+				return true;
+			}
 		}
 		return false;
 	}
@@ -47,12 +57,17 @@ public class ApontamentoServiceImpl implements ApontamentoService {
 
 	@Override
 	public List<Object> listarApontamentos() {
-		return dao.listarTudo();
+		return apontamentoDAO.listarTudo();
 	}
 
 	@Override
 	public Object buscarApontamentoPorId(Apontamento apontamento, long idApontamento) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public int calcularHorasTrabalhadas(Calendar hora1, Calendar hora2) {
+		int horasTrabalhadas = (hora1.get(Calendar.HOUR_OF_DAY) - hora2.get(Calendar.HOUR_OF_DAY));
+		return horasTrabalhadas;
 	}
 }
