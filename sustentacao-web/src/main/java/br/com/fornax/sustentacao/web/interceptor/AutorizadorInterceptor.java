ed793127373622aafacfc6604309a417dcfd967c
Danilo Righetto <danilo.righetto@fornax.com.br>
2016-04-25 11:06:30 -0300

    Resolvendo erro de logout

--- sustentacao-web/src/main/java/br/com/fornax/sustentacao/web/interceptor/AutorizadorInterceptor.java
+++ sustentacao-web/src/main/java/br/com/fornax/sustentacao/web/interceptor/AutorizadorInterceptor.java
@@ -16,6 +16,7 @@ public class AutorizadorInterceptor extends HandlerInterceptorAdapter{
 	      }
 	      else if(request.getRequestURI().equals("/sustentacao/logout")){
 	    	  request.getSession().invalidate();
+	    	  request.getSession().removeAttribute("usuario");
 	      }
 	      	response.sendRedirect("/sustentacao/painel");
 	      return false;
